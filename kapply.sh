f7cbf32d7a9883cb1b854f3e9f9bb552a8824b66
kimkun07 <kimkun07@naver.com>
2024-08-16 19:12:34 +0900

    kustomize로 gke/minikube 구분. 0.4.2 사용: secret을 이용해 backend에 주입

--- kapply.sh
+++ kapply.sh
@@ -1,1 +1,9 @@
-kubectl apply -k .
+if [[ "$1" == "minikube" || "$1" == "gke" ]]; then
+  kubectl config use-context "$1"
+  kubectl delete secret --all
+  kubectl create secret generic secret-backend\
+    --from-file=.secret=./.secret.backend # secret-backend[".secret"] <= ./.secret.backend file from local
+  kubectl apply -k ./overlays/"$1"
+else
+  echo "Usage: ./kapply.sh minikube|gke"
+fi
