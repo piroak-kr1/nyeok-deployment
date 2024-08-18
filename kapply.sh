cd059b68e9aeb392f4bfa2b0200464e95def521a
kimkun07 <kimkun07@naver.com>
2024-08-18 21:22:10 +0900

    - GKE에서 gateway 대신 cloudflare tunnel로 접속. nginx pod 제거 - minikube에서는 ingress 사용

--- kapply.sh
+++ kapply.sh
@@ -1,8 +1,9 @@
 if [[ "$1" == "minikube" || "$1" == "gke" ]]; then
   kubectl config use-context "$1"
   kubectl delete secret --all
-  kubectl create secret generic secret-backend\
-    --from-file=.secret=./.secret.backend # secret-backend[".secret"] <= ./.secret.backend file from local
+  kubectl create secret generic cluster-secret \
+    --from-file=secret-backend=./.secret.backend \
+    --from-file=cloudflare-token=./.secret.cloudflare
   kubectl apply -k ./overlays/"$1"
 else
   echo "Usage: ./kapply.sh minikube|gke"
